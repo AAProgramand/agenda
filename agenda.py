a3707c75d380ed6595ebf4c4abbe8f1214627fa9
liverO <70497019+Oliver1096@users.noreply.github.com>
2020-09-09 20:38:39 -0500

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -95,15 +95,5 @@ while True:
     else: 
         os.system("cls")
         print("Opcion no valida")
-        
-
-        
-                     
-echo "directorio" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/Oliver1096/directorio.git
-git push -u origin master
+                      
                 
